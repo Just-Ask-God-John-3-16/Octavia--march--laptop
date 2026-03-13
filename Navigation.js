a2d1235de7962c7802abfc7cc972bcb13464e6ce
Venusbaby1999 <tankntiff77@gmail.com>
2026-03-13 17:41:39 -0500

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
 import HomeScreen from './screens/HomeScreen';
+import DailyScriptureScreen from './screens/DailyScriptureScreen';
+import PrayerScreen from './screens/PrayerScreen';
+import BibleStudyScreen from './screens/BibleStudyScreen';
+import TestimonyScreen from './screens/TestimonyScreen';
+import RecoverySupportScreen from './screens/RecoverySupportScreen';
+import DonationsScreen from './screens/DonationsScreen';
+import AboutMinistryScreen from './screens/AboutMinistryScreen';
 
 const Stack = createStackNavigator();
 
