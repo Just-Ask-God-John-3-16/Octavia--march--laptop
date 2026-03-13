84373e5b821321baf3daac3962d57850c226b034
Venusbaby1999 <tankntiff77@gmail.com>
2026-03-13 17:42:41 -0500

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -11,6 +11,26 @@ import TestimonyScreen from './screens/TestimonyScreen';
 import RecoverySupportScreen from './screens/RecoverySupportScreen';
 import DonationsScreen from './screens/DonationsScreen';
 import AboutMinistryScreen from './screens/AboutMinistryScreen';
+const Stack = createStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DailyScripture" component={DailyScriptureScreen} />
+        <Stack.Screen name="Prayer" component={PrayerScreen} />
+        <Stack.Screen name="BibleStudy" component={BibleStudyScreen} />
+        <Stack.Screen name="Testimony" component={TestimonyScreen} />
+        <Stack.Screen name="Recovery" component={RecoverySupportScreen} />
+        <Stack.Screen name="Donations" component={DonationsScreen} />
+        <Stack.Screen name="AboutMinistry" component={AboutMinistryScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const Stack = createStackNavigator();
 
