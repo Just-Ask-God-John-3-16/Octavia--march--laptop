352e30e78af5e7ce2fb41d98c5ac9cb6731aa89f
Venusbaby1999 <tankntiff77@gmail.com>
2026-03-14 00:28:25 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navigation from './Navigation';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
-  return <Navigation />;
+  return <HomeScreen />;
 }
