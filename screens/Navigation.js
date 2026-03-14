e510ac0c2024b264d4b9686a6d0f4a7cf80a62bb
Venusbaby1999 <tankntiff77@gmail.com>
2026-03-13 19:18:59 -0500

    Update Navigation.js

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -13,6 +13,12 @@ export default function Navigation() {
    <Stack.Screen name="Dashboard" component={Dashboard} /> 
         <Stack.Screen 
           name="Home" 
+<Stack.Screen name="DailyScripture" component={DailyScriptureScreen} />
+<Stack.Screen name="Prayer" component={PrayerScreen} />
+<Stack.Screen name="BibleStudy" component={BibleStudyScreen} />
+<Stack.Screen name="Testimony" component={TestimonyScreen} />
+<Stack.Screen name="Recovery" component={RecoverySupportScreen} />
+<Stack.Screen name="Donations" component={DonationsScreen} />  
           component={HomeScreen} 
           options={{ title: 'Just Ask God Ministries' }}
         />
