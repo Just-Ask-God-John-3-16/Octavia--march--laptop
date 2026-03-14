838206c8dc2bd0f1bd77f01386ef060ed3c0faad
Venusbaby1999 <tankntiff77@gmail.com>
2026-03-13 19:23:17 -0500

    Update Navigation.js

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -2,7 +2,15 @@ import Dashboard from './screens/Dashboard';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+// Import all your screens
 import HomeScreen from './screens/HomeScreen';
+import DailyScriptureScreen from './screens/DailyScriptureScreen';
+import PrayerScreen from './screens/PrayerScreen';
+import BibleStudyScreen from './screens/BibleStudyScreen';
+import TestimonyScreen from './screens/TestimonyScreen';
+import RecoverySupportScreen from './screens/RecoverySupportScreen';
+import DonationsScreen from './screens/DonationsScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,18 +18,23 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-   <Stack.Screen name="Dashboard" component={Dashboard} /> 
+
+        {/* Dashboard is the home screen */}
         <Stack.Screen 
-          name="Home" 
-<Stack.Screen name="DailyScripture" component={DailyScriptureScreen} />
-<Stack.Screen name="Prayer" component={PrayerScreen} />
-<Stack.Screen name="BibleStudy" component={BibleStudyScreen} />
-<Stack.Screen name="Testimony" component={TestimonyScreen} />
-<Stack.Screen name="Recovery" component={RecoverySupportScreen} />
-<Stack.Screen name="Donations" component={DonationsScreen} />  
-          component={HomeScreen} 
-          options={{ title: 'Just Ask God Ministries' }}
+          name="Dashboard" 
+          component={Dashboard} 
+          options={{ headerShown: false }}
         />
+
+        {/* Other screens */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DailyScripture" component={DailyScriptureScreen} />
+        <Stack.Screen name="Prayer" component={PrayerScreen} />
+        <Stack.Screen name="BibleStudy" component={BibleStudyScreen} />
+        <Stack.Screen name="Testimony" component={TestimonyScreen} />
+        <Stack.Screen name="Recovery" component={RecoverySupportScreen} />
+        <Stack.Screen name="Donations" component={DonationsScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
